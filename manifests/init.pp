ce1ce94c06e6a26c8a0e1894d640310d59e0eceb
teyo <teyo@puppetlabs.com>
2012-06-11 06:16:15 +0000

    Creates the structure for the Puppet Module that will set up the GCE tools and Puppet Cloud Provisioner to work with GCE

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: node_gce
+#
+# This module manages node_gce
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class node_gce {
+
+
+}
