044d33f3fedfd885b57b513332afa3ad0702817e
Rick Bradley <rick@rickbradley.com>
2012-06-12 14:05:52 -0500

    Re-word spec descriptions to be more behavioral

--- spec/unit/puppet/face/node_gce_spec.rb
+++ spec/unit/puppet/face/node_gce_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Puppet::Face[:node_gce, :current] do
-  it 'supports calling create' do
+  it 'supports creating an instance' do
     lambda { subject.create }.should_not raise_error
   end
 
-  it 'supports calling project' do
+  it 'supports retrieving project data' do
     lambda { subject.project }.should_not raise_error
   end
 end
