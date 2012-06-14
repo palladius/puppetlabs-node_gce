d0c66aed927a0b3d9ef3cda84806929ab6f3dbb7
Rick Bradley <rick@rickbradley.com>
2012-06-14 15:53:22 -0500

    Enable commented specs

--- spec/integration/project_spec.rb
+++ spec/integration/project_spec.rb
@@ -25,35 +25,35 @@ describe 'when retrieving project data' do
     @handle.stubs(:fetch_credentials).returns(credentials)
   end
 
-  # it 'fails when there is no project name' do
-  #   options.delete(:project)
-  #   lambda { face.project(options) }.should raise_error
-  # end
-  #
-  # it 'fails when there is no credentials data' do
-  #   @handle.stubs(:fetch_credentials).returns({})
-  #   lambda { face.project(options) }.should raise_error
-  # end
-  #
-  # it 'fails when the credentials data does not include a client id' do
-  #   credentials.delete(:client_id)
-  #   lambda { face.project(options) }.should raise_error
-  # end
-  #
-  # it 'fails when the credentials data does not include a client secret' do
-  #   credentials.delete(:client_secret)
-  #   lambda { face.project(options) }.should raise_error
-  # end
-  #
-  # it 'fails when the credentials data does not include a refresh token' do
-  #   credentials.delete(:refresh_token)
-  #   lambda { face.project(options) }.should raise_error
-  # end
-  #
-  # it 'fails when the credentials provided are invalid' do
-  #   credentials[:client_id] = '1462647242-bad-id.apps.googleusercontent.com'
-  #   lambda { face.project(options) }.should raise_error
-  # end
+  it 'fails when there is no project name' do
+    options.delete(:project)
+    lambda { face.project(options) }.should raise_error
+  end
+
+  it 'fails when there is no credentials data' do
+    @handle.stubs(:fetch_credentials).returns({})
+    lambda { face.project(options) }.should raise_error
+  end
+
+  it 'fails when the credentials data does not include a client id' do
+    credentials.delete(:client_id)
+    lambda { face.project(options) }.should raise_error
+  end
+
+  it 'fails when the credentials data does not include a client secret' do
+    credentials.delete(:client_secret)
+    lambda { face.project(options) }.should raise_error
+  end
+
+  it 'fails when the credentials data does not include a refresh token' do
+    credentials.delete(:refresh_token)
+    lambda { face.project(options) }.should raise_error
+  end
+
+  it 'fails when the credentials provided are invalid' do
+    credentials[:client_id] = '1462647242-bad-id.apps.googleusercontent.com'
+    lambda { face.project(options) }.should raise_error
+  end
 
   it 'returns the project data from the Google Compute API' do
     face.project(options).should == '{}'
