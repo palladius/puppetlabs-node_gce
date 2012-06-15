273faa516c2ffe24346133817d204af200c4dce7
Rick Bradley <rick@rickbradley.com>
2012-06-15 12:47:12 -0500

    Remove whitespace in face project action file

--- lib/puppet/face/node_gce/project.rb
+++ lib/puppet/face/node_gce/project.rb
@@ -1,9 +1,7 @@
 require 'puppet/face/node_gce'
 
 Puppet::Face.define :node_gce, '0.0.1' do
-
   action :project do
-
     summary 'Return information on the project in question.'
     description <<-EOT
       Return data about the project instance.
