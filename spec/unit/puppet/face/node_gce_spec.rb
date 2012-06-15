3d488875a7c148c08d669b9a2b4cb0efb3e4f02c
Rick Bradley <rick@rickbradley.com>
2012-06-15 12:45:59 -0500

    Add unit spec for face 'list' action

--- spec/unit/puppet/face/node_gce_spec.rb
+++ spec/unit/puppet/face/node_gce_spec.rb
@@ -10,6 +10,35 @@ describe Puppet::Face[:node_gce, :current] do
     lambda { subject.create }.should_not raise_error
   end
 
+  describe 'when listing instance data' do
+    let :options do
+      { :project => 'megaproject' }
+    end
+
+    it 'requires a project name' do
+      lambda {
+        options.delete[:project]
+        subject.list(options)
+      }.should raise_error(ArgumentError)
+    end
+
+    describe 'and a project name is available' do
+      it 'requests running instance list data for the given project via the Google Compute REST API' do
+        @handle.expects(:instance_list).with(options[:project])
+        subject.list(options)
+      end
+
+      it 'returns the running instance list data provided by the Google Compute REST API' do
+        @handle.stubs(:instance_list).returns( {:foo => 'bar'} )
+        subject.list(options).should == {:foo => 'bar'}
+      end
+
+      it 'outputs the running instance list data provided by the Google Compute REST API to the console' do
+        pending("figuring out how to test the console output hook")
+      end
+    end
+  end
+
   describe 'when retrieving project data' do
     let :options do
       {
