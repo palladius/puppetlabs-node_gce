68dd0c51653a664aceda0d58d0dbb39ec6b0dfa3
Rick Bradley <rick@rickbradley.com>
2012-06-15 14:24:55 -0500

    Add create action unit test

--- spec/unit/puppet/face/node_gce_spec.rb
+++ spec/unit/puppet/face/node_gce_spec.rb
@@ -7,12 +7,41 @@ describe Puppet::Face[:node_gce, :current] do
 
   before do
     @handle = Puppet::GoogleCompute.new(options[:project])
-    @handle.stubs(:get).returns('')
     Puppet::GoogleCompute.stubs(:new).returns(@handle)
+
+    # neuter live API requests
+    @handle.stubs(:get).returns('')
+    @handle.stubs(:post).returns('')
   end
 
-  it 'supports creating an instance' do
-    lambda { subject.create }.should_not raise_error
+  describe 'when creating an instance' do
+    it 'requires a project name' do
+      lambda {
+        options.delete[:project]
+        subject.create(options)
+      }.should raise_error(ArgumentError)
+    end
+
+    describe 'and a project name is available' do
+      it 'provides the project name to the Google Compute REST API' do
+        Puppet::GoogleCompute.expects(:new).with(options[:project]).returns(@handle)
+        subject.create(options)
+      end
+
+      it 'requests running instance list data for the given project via the Google Compute REST API' do
+        @handle.expects(:instance_create).with(options)
+        subject.create(options)
+      end
+
+      it 'returns the running instance list data provided by the Google Compute REST API' do
+        @handle.stubs(:instance_create).returns( {:foo => 'bar'} )
+        subject.create(options).should == {:foo => 'bar'}
+      end
+
+      it 'outputs the running instance list data provided by the Google Compute REST API to the console' do
+        pending("figuring out how to test the console output hook")
+      end
+    end
   end
 
   describe 'when listing instance data' do
