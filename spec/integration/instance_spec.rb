44229a90d6e223f579b2cc59da4c73e97b8eb276
Rick Bradley <rick@rickbradley.com>
2012-06-15 14:24:33 -0500

    Share common setup in instance integration spec

--- spec/integration/instance_spec.rb
+++ spec/integration/instance_spec.rb
@@ -5,7 +5,7 @@ def fixture_path(file)
   File.expand_path(File.join(File.dirname(__FILE__), '..', 'fixtures', file))
 end
 
-describe 'when retrieving instance list data' do
+describe 'instances' do
   let :face do
     Puppet::Face[:node_gce, :current]
   end
@@ -22,36 +22,72 @@ describe 'when retrieving instance list data' do
     @handle = Puppet::GoogleCompute.new(options[:project])
     Puppet::GoogleCompute.stubs(:new).returns(@handle)
     @handle.stubs(:fetch_credentials).returns(credentials)
+    options.merge!({})
   end
 
-  it 'fails when there is no credentials data' do
-    @handle.stubs(:fetch_credentials).returns({})
-    lambda { face.list(options) }.should raise_error
+  describe 'when creating an instance' do
+    it 'fails when there is no credentials data' do
+      @handle.stubs(:fetch_credentials).returns({})
+      lambda { face.create(options) }.should raise_error
+    end
+
+    it 'fails when the credentials data does not include a client id' do
+      credentials.delete(:client_id)
+      lambda { face.create(options) }.should raise_error
+    end
+
+    it 'fails when the credentials data does not include a client secret' do
+      credentials.delete(:client_secret)
+      lambda { face.create(options) }.should raise_error
+    end
+
+    it 'fails when the credentials data does not include a refresh token' do
+      credentials.delete(:refresh_token)
+      lambda { face.create(options) }.should raise_error
+    end
+
+    it 'fails when the credentials provided are invalid' do
+      credentials[:client_id] = '1462647242-bad-id.apps.googleusercontent.com'
+      lambda { face.create(options) }.should raise_error
+    end
+
+    it 'returns operation data from the Google Compute API' do
+      json_result = face.create(options)
+      result = PSON.parse(json_result)  # yeah, I know.  "PSON" was not my decision.
+      result.keys.sort.should == ["id", "kind", "selfLink"]
+    end
   end
 
-  it 'fails when the credentials data does not include a client id' do
-    credentials.delete(:client_id)
-    lambda { face.list(options) }.should raise_error
-  end
+  describe 'when retrieving instance list data' do
+    it 'fails when there is no credentials data' do
+      @handle.stubs(:fetch_credentials).returns({})
+      lambda { face.list(options) }.should raise_error
+    end
 
-  it 'fails when the credentials data does not include a client secret' do
-    credentials.delete(:client_secret)
-    lambda { face.list(options) }.should raise_error
-  end
+    it 'fails when the credentials data does not include a client id' do
+      credentials.delete(:client_id)
+      lambda { face.list(options) }.should raise_error
+    end
 
-  it 'fails when the credentials data does not include a refresh token' do
-    credentials.delete(:refresh_token)
-    lambda { face.list(options) }.should raise_error
-  end
+    it 'fails when the credentials data does not include a client secret' do
+      credentials.delete(:client_secret)
+      lambda { face.list(options) }.should raise_error
+    end
 
-  it 'fails when the credentials provided are invalid' do
-    credentials[:client_id] = '1462647242-bad-id.apps.googleusercontent.com'
-    lambda { face.list(options) }.should raise_error
-  end
+    it 'fails when the credentials data does not include a refresh token' do
+      credentials.delete(:refresh_token)
+      lambda { face.list(options) }.should raise_error
+    end
 
-  it 'returns the instance list data from the Google Compute API' do
-    json_result = face.list(options)
-    result = PSON.parse(json_result)  # yeah, I know.  "PSON" was not my decision.
-    result.keys.sort.should == ["id", "kind", "selfLink"]
+    it 'fails when the credentials provided are invalid' do
+      credentials[:client_id] = '1462647242-bad-id.apps.googleusercontent.com'
+      lambda { face.list(options) }.should raise_error
+    end
+
+    it 'returns the instance list data from the Google Compute API' do
+      json_result = face.list(options)
+      result = PSON.parse(json_result)  # yeah, I know.  "PSON" was not my decision.
+      result.keys.sort.should == ["id", "kind", "selfLink"]
+    end
   end
 end
